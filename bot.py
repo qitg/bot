b0c2b74ef3932e93688f8fafd2b4f1b4d3f258bd
qitg <169071476+qitg@users.noreply.github.com>
2026-05-14 12:08:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ import threading
 from flask import Flask, render_template_string, request
 
 # ========== КОНФИГ ==========
-TOKEN = "8307596159:AAES-a6TjEaAaP_j6LPogq2Eb9vsoBqtL4o"
+TOKEN = "8307596159:AAGkuxqO1WKToY_9k6nXegDqljFH45L-mmQ
+"
 ADMIN_ID = 7072265211
 
 bot = telebot.TeleBot(TOKEN)
